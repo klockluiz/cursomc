fd14cae856cc07a82b52f9d1bd1f1f7821d0820b
Luiz Klock <klockluiz@gmail.com>
2021-07-31 21:59:09 -0300

    Criando operacao de instanciacao

--- src/main/java/com/luiz/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/luiz/cursomc/repositories/CategoriaRepository.java
@@ -2,7 +2,6 @@ package com.luiz.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.luiz.cursomc.domain.Categoria;
 
 @Repository
